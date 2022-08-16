348b65987710428b0752d352e2d3696827257f82
darshikvadadoria <darshikpatel29011@gmail.com>
2022-08-16 20:35:32 +0530

    about design

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
 import styles from './about-us.module.scss'
 import Image from 'next/image';
 import about1 from '../public/assets/about-1.png'
 import about2 from '../public/assets/about-2.png'
 import handShakeImg from '../public/assets/handshake.png'
 import phoneImg from '../public/assets/phone.svg'
-
+import Image1 from '../public/assets/Image1.png'
 
 export default function aboutus() {
   return (
@@ -53,6 +54,36 @@ export default function aboutus() {
           </div>
         </div>
       </div>
+
+      <div className={styles.storyImage}>
+        <div className={styles.container}>
+          <div className={styles.storybox}>
+
+            <div className={styles.boximagedesign}></div>
+            <div className={styles.boxstorybg}>
+              <h4> <span>Our</span> Story</h4>
+              <p>tever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+
+          </div>
+
+        <div className={styles.secoundstorybox}>
+           <div className={styles.storybox}>
+
+            <div className={styles.boximagedesign}></div>
+            <div className={styles.boxstorybg}>
+            
+              <h4> <span>Who</span>  We Are?</h4>
+              <p>tever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+
+          </div>
+        </div>
+
+         
+        </div>
+      </div>
+
       <div className={styles.container}>
         <div className={styles.teamWrapper}>
           <h4 className={styles.header}>Our Team</h4>
@@ -118,6 +149,7 @@ export default function aboutus() {
           </div>
         </div>
       </div>
+      <Footer/>
     </div>
   )
 }
