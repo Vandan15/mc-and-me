d7742f48dfbe57c72a5d9e6489be60d9ceb7f4af
vandan khamkar <vandankhamkar1@gmail.com>
2022-08-16 16:55:59 +0530

    add about section

--- pages/about-us.js
+++ pages/about-us.js
@@ -4,6 +4,8 @@ import styles from './about-us.module.scss'
 import Image from 'next/image';
 import about1 from '../public/assets/about-1.png'
 import about2 from '../public/assets/about-2.png'
+import handShakeImg from '../public/assets/handshake.png'
+import phoneImg from '../public/assets/phone.svg'
 
 
 export default function aboutus() {
@@ -27,7 +29,7 @@ export default function aboutus() {
         <div className={styles.container}>
           <div className={styles.grid}>
             <div className={styles.subgrid}>
-              <div className={styles.Firstimg}> 
+              <div className={styles.Firstimg}>
                 <Image src={about1} />
               </div>
             </div>
@@ -35,10 +37,10 @@ export default function aboutus() {
             <div className={styles.subgrid}>
               <div className={styles.secoundimg}>
                 <Image src={about2} />
-               
+
               </div>
-              <div  className={styles.secoundimg}>
-              <Image src={about2} />
+              <div className={styles.secoundimg}>
+                <Image src={about2} />
               </div>
             </div>
 
@@ -51,11 +53,71 @@ export default function aboutus() {
           </div>
         </div>
       </div>
-
-    <div>
-      
-    </div>
-
+      <div className={styles.container}>
+        <div className={styles.teamWrapper}>
+          <h4 className={styles.header}>Our Team</h4>
+          <div className={styles.teamMember}>
+            <div className={styles.cardWrapper}>
+              <h4>Jeremy Mare</h4>
+              <p>Chief Executive Officer</p>
+              <Image src={handShakeImg} />
+            </div>
+            <div className={styles.cardWrapper}>
+              <h4>Jeremy Mare</h4>
+              <p>Chief Executive Officer</p>
+              <Image src={handShakeImg} />
+            </div>
+            <div className={styles.cardWrapper}>
+              <h4>Jeremy Mare</h4>
+              <p>Chief Executive Officer</p>
+              <Image src={handShakeImg} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.teamValue}>
+          <h4 className={styles.header}>Our Values</h4>
+          <div className={styles.teamMember}>
+            <div className={styles.cardWrapper}>
+              <div>
+                <Image src={phoneImg} />
+              </div>
+              <div className={styles.contentWrap}>
+                <h4>Communicate</h4>
+                <p>Good communicator as we have large number of good team who constantly support out user</p>
+              </div>
+            </div>
+            <div className={styles.cardWrapper}>
+              <div>
+                <Image src={phoneImg} />
+              </div>
+              <div className={styles.contentWrap}>
+                <h4>Communicate</h4>
+                <p>Good communicator as we have large number of good team who constantly support out user</p>
+              </div>
+            </div>
+            <div className={styles.cardWrapper}>
+              <div>
+                <Image src={phoneImg} />
+              </div>
+              <div className={styles.contentWrap}>
+                <h4>Communicate</h4>
+                <p>Good communicator as we have large number of good team who constantly support out user</p>
+              </div>
+            </div>
+            <div className={styles.cardWrapper}>
+              <div>
+                <Image src={phoneImg} />
+              </div>
+              <div className={styles.contentWrap}>
+                <h4>Communicate</h4>
+                <p>Good communicator as we have large number of good team who constantly support out user</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
